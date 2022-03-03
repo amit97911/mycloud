e85d325d9bcfd4ebdab0bcfc8843a1550e6e7524
amit97911 <amit97911@gmail.com>
2022-03-03 17:08:58 +0530

    Modified to display error message and progress bar resets if the same page is slected over and over to uplaod files

--- app/Http/Controllers/UploadHandler.php
+++ app/Http/Controllers/UploadHandler.php
@@ -16,28 +16,13 @@ class UploadHandler extends Controller
     public function upload(Request $request)
     {
         $data['active'] = "upload";
-        $request->validate([
-            'upload_file' => 'required',
-        ]);
         if ($request->hasFile('upload_file')) {
-            $allowedfileExtension = ['pdf', 'jpg', 'png', 'docx','mp4'];
-            $files = $request->file('upload_file');
-            foreach ($files as $file) {
-                $filename = $file->getClientOriginalName();
-                $extension = $file->getClientOriginalExtension();
-                $check = in_array($extension, $allowedfileExtension);
-                if ($check) {
-                    foreach ($request->upload_file as $photo) {
-                        $filename = $photo->store('upload_file');
-                    }
-                    event(new MyEvent('Uploaded'.$filename));
-                } else {
-                    event(new MyEvent('Error uploading'.$filename));
-                }
+            foreach ($request->upload_file as $photo) {
+                $photo->storeAs('upload_file',$photo->getClientOriginalName());
+                event(new MyEvent('Uploaded : ' .$photo->getClientOriginalName()));
             }
         }
         return view('upload.index', $data);
-
     }
 
     // public function upload(Request $request)
--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -4,8 +4,8 @@
         <form id="fileUploadForm" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="form-group mb-3">
-                <input name="upload_file[]" type="file" class="form-control" multiple>
-                <div class="text-danger" id="error_upload_file"></div>
+                <input name="upload_file[]" type="file" class="form-control" multiple required>
+                <div id="error" class="text-danger"></div>
             </div>
             <div class="d-grid mb-3">
                 <input type="submit" value="Submit" class="btn btn-primary" id="submit-upload">
@@ -22,26 +22,79 @@
 
 
     <script>
-        $(function () {
-            $(document).ready(function () {
-                $('#fileUploadForm').ajaxForm({
-                    beforeSend: function () {
-                        var percentage = '0';
-                    },
-                    uploadProgress: function (event, position, total, percentComplete) {
-                        var percentage = percentComplete;
-                        $('.progress .progress-bar').css("width", percentage+'%', function() {
-                          return $(this).attr("aria-valuenow", percentage) + "%";
-                        })
-                        $('.progress .progress-bar').html(percentage+'%');
-                    },
-                    complete: function (xhr) {
-                        console.log('File has uploaded');
-                    }
-                });
-            });
+        $(document).ready(function() {
+            $('.progress').hide();
         });
-        
-    </script>
 
+        $('#fileUploadForm').ajaxForm({
+            beforeSend: function() {
+                var percentage = '0';
+                $('.progress').show();
+                $('.progress .progress-bar').css("width", percentage + '%', function() {
+                    return $(this).attr("aria-valuenow", percentage) + "%";
+                });
+            },
+            uploadProgress: function(event, position, total, percentComplete) {
+                var percentage = percentComplete;
+                $('.progress .progress-bar').css("width", percentage + '%', function() {
+                    return $(this).attr("aria-valuenow", percentage) + "%";
+                });
+            },
+            complete: function(xhr) {
+                if (xhr.status == 422) {
+                    $('.progress').hide();
+                    $('#error').html("Error : " + xhr.responseJSON.message);
+                }
+            }
+        });
+
+
+        // $('#submit-upload').click(function() {
+        //     $('#fileUploadForm').ajaxForm({
+        //         beforeSend: function() {
+        //             var percentage = '0';
+        //             $('.progress').show();
+        //             $('.progress .progress-bar').css("width", percentage + '%', function() {
+        //                 return $(this).attr("aria-valuenow", percentage) + "%";
+        //             })
+        //             $('.progress .progress-bar').html(percentage + '%');
+        //         },
+        //         uploadProgress: function(event, position, total, percentComplete) {
+        //             var percentage = percentComplete;
+        //             $('.progress .progress-bar').css("width", percentage + '%', function() {
+        //                 return $(this).attr("aria-valuenow", percentage) + "%";
+        //             })
+        //             $('.progress .progress-bar').html(percentage + '%');
+        //         },
+        //         complete: function(xhr) {
+        //             if (xhr.status == 422) {
+        //                 $('.progress').hide();
+        //                 $('#error').html("Error : " + xhr.responseJSON.message);
+        //             }
+        //         }
+        //     });
+        // });
+
+        // $(function() {
+        //     $(document).ready(function() {
+        //         $('#progress').hide();
+        //         $('#fileUploadForm').ajaxForm({
+        //             beforeSend: function() {
+        //                 var percentage = '0';
+        //             },
+        //             uploadProgress: function(event, position, total, percentComplete) {
+        //                 $('#progress').show();
+        //                 var percentage = percentComplete;
+        //                 $('.progress .progress-bar').css("width", percentage + '%', function() {
+        //                     return $(this).attr("aria-valuenow", percentage) + "%";
+        //                 })
+        //                 $('.progress .progress-bar').html(percentage + '%');
+        //             },
+        //             complete: function(xhr) {
+        //                 console.log('File has uploaded');
+        //             }
+        //         });
+        //     });
+        // });
+    </script>
 @stop
