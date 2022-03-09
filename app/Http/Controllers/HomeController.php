32bcd408b9860904b2028005204186d14587a246
amit97911 <amit97911@gmail.com>
2022-03-10 01:05:22 +0530

    Download file function at HomeController -> index() modified

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use ZipArchive;
 use File;
+use App\Http\Resources\MyEvent;
 
 class HomeController extends Controller
 {
@@ -12,15 +13,15 @@ class HomeController extends Controller
     {
     	$zip = new ZipArchive;
         $fileName = 'zipFileName.zip';
-        if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE)
+        if ($zip->open(storage_path('app/public/'.$fileName), ZipArchive::CREATE) === TRUE)
         {
-            $files = File::files(storage_path('app/upload_file'));
+            $files = File::files(storage_path('app/public/upload_file'));
             foreach ($files as $key => $value) {
                 $relativeNameInZipFile = basename($value);
                 $zip->addFile($value, $relativeNameInZipFile);
             }
             $zip->close();
         }
-        return response()->download(public_path($fileName));
+        return response()->download(storage_path('app/public/'.$fileName));
     }
 }
